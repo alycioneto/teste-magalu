477cfc504674a3b8d4953da7f2ba4326afaffe4b
Alycio <alycio.neto@olist.com>
2021-11-12 15:04:48 -0300

    feat: request util create a simple request interface

---
+++ cep-service/src/shared/utils/Request.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+
+class Request {
+  private api: AxiosInstance
+
+  private config: AxiosRequestConfig
+
+  constructor(baseUrl: string, config?: AxiosRequestConfig) {
+    this.config = this.getConfig(baseUrl)
+    this.api = axios.create({ ...this.config, ...config })
+  }
+
+  public async get(url: string, data?: object, config?: AxiosRequestConfig): Promise<AxiosResponse> {
+    config = { ...config, params: data }
+
+    return this.api.get(`${url}`, config)
+  }
+
+  private getConfig(baseUrl: string): AxiosRequestConfig {
+    return {
+      baseURL: baseUrl,
+      headers: {
+        common: { 'Content-Type': 'application/json' },
+      },
+    }
+  }
+
+}
+
+export { Request }
