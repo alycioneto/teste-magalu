4bb0d2e7fa8362c0a99cbe3f2c8c9be3bf15b926
Alycio <alycio.neto@olist.com>
2021-11-15 19:52:09 -0300

    test: add test to StringUtil

---
+++ cep-service/src/shared/utils/String.spec.ts
@@ -0,0 +1,46 @@
+import { StringUtil } from "./String"
+
+interface SutTypes {
+  sut: StringUtil
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new StringUtil()
+
+  return { sut }
+}
+
+describe('Tests for "StringUtil" class', () => {
+
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
+
+  describe('Tests for the "replaceAt" method', () => {
+
+    it('Should return 90220010 when called with 90220012 on cep 7 on index and 0 on value', async () => {
+      const response = StringUtil.replaceAt("90220012", 7, "0")
+
+      expect(response).toBe("90220010")
+    })
+
+    it('Should return TESTE when called with TOSTE on cep 7 on index and E on value', async () => {
+      const response = StringUtil.replaceAt("TOSTE", 1, "E")
+
+      expect(response).toBe("TESTE")
+    })
+
+    it('Should throw an error when index grather or equal than string length', async () => {
+      expect( () => {
+        StringUtil.replaceAt("90220012", 8, "0")
+      }).toThrow()
+    })
+
+    it('Should throw an error when index lesss than ZERO', async () => {
+      expect( () => {
+        StringUtil.replaceAt("90220012", -1, "0")
+      }).toThrow()
+    })
+  })
+})
--- cep-service/src/shared/utils/String.ts
+++ cep-service/src/shared/utils/String.ts
@@ -1,5 +1,8 @@
 class StringUtil {
   public static replaceAt(string: string, index: number, value: string): string {
+    if(index >= string.length || index < 0) {
+      throw new Error()
+    }
     return string.substring(0, index) + value + string.substring(index + 1);
   }
 }
