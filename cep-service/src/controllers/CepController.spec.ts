95ebf679862897ef1a7181445ab8892fcb0bc1e3
Alycio <alycio.neto@olist.com>
2021-11-15 18:02:05 -0300

    test: add testes to token and cep controller

---
+++ cep-service/src/controllers/CepController.spec.ts
@@ -0,0 +1,113 @@
+import { ICepService } from './../types/ICepService'
+import { getMockReq, getMockRes } from '@jest-mock/express'
+import { Response } from 'express'
+import { CepController } from '../controllers'
+import { CepServiceResponse } from '../types'
+import HttpStatus from 'http-status-codes'
+
+const { VIACEP_BASE_URL = "https://viacep.com.br/" } = process.env
+interface SutTypes {
+  sut: CepController
+  service: ICepService
+  cep: string
+}
+
+const makeService = (): ICepService => {
+  class ServiceFake implements ICepService {
+    get(cep: string): Promise<CepServiceResponse> {
+      return Promise.resolve({
+        rua: 'Rua Santo Antônio',
+        bairro: 'Floresta',
+        cidade: 'Porto Alegre',
+        estado: 'RS'
+      })
+    }
+  }
+
+  return new ServiceFake()
+}
+
+const makeSut = (): SutTypes => {
+  const service = makeService()
+  const sut = new CepController("/cep/:cep", service)
+  const cep = '90220010'
+
+  return { sut, service, cep }
+}
+
+
+let res: Response
+describe('Tests for "CepController" class', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+
+    const response = getMockRes()
+    res = response.res
+  })
+
+  describe('Tests for the "get" method', () => {
+    it('Return an error when cep not sent in params', async () => {
+      const req = getMockReq({
+        params: {},
+      })
+
+      const { sut } = makeSut()
+
+      const { error } = sut.getSchema().params.validate(req.params, sut.validatorConfig.joi)
+      expect(error!.details.length).toBe(1)
+      expect(error!.details[0].message).toBe('"cep" is required')
+    })
+
+    it('Return an error when cep is not a string with only numbers', async () => {
+      const req = getMockReq({
+        params: {
+          cep: "test",
+        },
+      })
+
+      const { sut } = makeSut()
+
+      const { error } = sut.getSchema().params.validate(req.params, sut.validatorConfig.joi)
+
+      expect(error!.details.length).toBe(1)
+      expect(error!.details[0].message).toBe('"cep" with value "test" fails to match the required pattern: /\\d{8}/')
+    })
+
+    it('Return an bad request when service returns an error', async () => {
+      const { sut, service, cep } = makeSut()
+
+      const req = getMockReq({
+        params: { cep },
+      })
+
+      const createSpy = jest.spyOn(service, 'get').mockRejectedValue(new Error())
+
+      const { error } = sut.getSchema().params.validate(req.params, sut.validatorConfig.joi)
+      res = await sut.get(req, res)
+
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.BAD_REQUEST)
+      expect(error).toBeUndefined()
+      expect(createSpy).toBeCalledWith(cep)
+    })
+
+    it('Expect a status OK when the flow is processed without errors', async () => {
+      const { sut, service, cep } = makeSut()
+
+      const req = getMockReq({
+        params: { cep },
+      })
+
+      const createSpy = jest.spyOn(service, 'get')
+
+      res = await sut.get(req, res)
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.OK)
+      expect(res.json).toHaveBeenCalledWith({
+        "bairro": "Floresta",
+        "cidade": "Porto Alegre",
+        "estado": "RS",
+        "rua": "Rua Santo Antônio",
+      })
+      expect(createSpy).toBeCalledWith(cep)
+    })
+  })
+})
---
+++ cep-service/src/controllers/TokenController.spec.ts
@@ -0,0 +1,114 @@
+import { IUserService } from '../types/IUserService'
+import { getMockReq, getMockRes } from '@jest-mock/express'
+import { Response } from 'express'
+import { TokenController } from '../controllers'
+import HttpStatus from 'http-status-codes'
+import { IUser } from '../types'
+
+interface SutTypes {
+  sut: TokenController
+  service: IUserService
+  email: string
+  password: string
+}
+
+const makeService = (): IUserService => {
+  class ServiceFake implements IUserService {
+    find(email: string, password: string): IUser | undefined {
+      return {
+        id: "1",
+        name: "John",
+        email: "john@mail.com",
+        password: "john123"
+      }
+    }
+  }
+
+  return new ServiceFake()
+}
+
+const makeSut = (): SutTypes => {
+  const service = makeService()
+  const sut = new TokenController("/token", service)
+  const email = "john@mail.com"
+  const password = "john123"
+
+  return { sut, service, email, password }
+}
+
+let res: Response
+describe('Tests for "TokenController" class', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+
+    const response = getMockRes()
+    res = response.res
+  })
+
+  describe('Tests for the "get" method', () => {
+    it('Return an error when email and password not sent in params', async () => {
+      const req = getMockReq({
+        body: {},
+      })
+
+      const { sut } = makeSut()
+
+      const { error } = sut.postSchema().body.validate(req.body, sut.validatorConfig.joi)
+      expect(error!.details.length).toBe(2)
+      expect(error!.details[0].message).toBe('"email" is required')
+      expect(error!.details[1].message).toBe('"password" is required')
+    })
+
+    it('Return an error when email is not a valid mail', async () => {
+      const { sut, password } = makeSut()
+
+      const req = getMockReq({
+        body: {
+          email: "test",
+          password
+        },
+      })
+
+
+      const { error } = sut.postSchema().body.validate(req.body, sut.validatorConfig.joi)
+
+      expect(error!.details.length).toBe(1)
+      expect(error!.details[0].message).toBe('"email" must be a valid email')
+    })
+
+    it('Return an unauthorized request when service returns not found a user', async () => {
+      const { sut, service, email, password } = makeSut()
+
+      const req = getMockReq({
+        body: { email, password },
+      })
+
+      const createSpy = jest.spyOn(service, 'find').mockReturnValueOnce(undefined)
+
+      const { error } = sut.postSchema().body.validate(req.body, sut.validatorConfig.joi)
+      res = await sut.post(req, res)
+
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.UNAUTHORIZED)
+      expect(error).toBeUndefined()
+      expect(createSpy).toBeCalledWith(email, password)
+    })
+
+    it('Expect a status OK when the flow is processed without errors', async () => {
+      const { sut, service, email, password } = makeSut()
+
+      const req = getMockReq({
+        body: { email, password },
+      })
+
+      const createSpy = jest.spyOn(service, 'find')
+
+      const { error } = sut.postSchema().body.validate(req.body, sut.validatorConfig.joi)
+      res = await sut.post(req, res)
+
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.OK)
+      expect(res.json).toHaveBeenCalled()
+      expect(error).toBeUndefined()
+      expect(createSpy).toBeCalled()
+    })
+  })
+})
